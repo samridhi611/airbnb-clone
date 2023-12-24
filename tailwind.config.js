69d9a8d38f52e25d4e19768bf4fff232f34a22c8
samridhi6112 <samridhi@wesoftek.com>
2023-12-24 09:07:56 +0530

    set-up-done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,js,jsx,tsx}",
+    "./pages/**/*.{ts,js,jsx,tsx}",
+    "./components/**/*.{ts,js,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
